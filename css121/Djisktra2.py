ded477e2963bc1b00df8a2b6760a9710d872f483
tententgc <nyman3527@gmail.com>
2021-11-25 14:10:00 +0700

    update djitest

--- css121/Djisktra2.py
+++ css121/Djisktra2.py
@@ -60,24 +60,24 @@ def dijkstra(graph, start, goal):
         min_distance_node = None
         
         for node in unseenNodes:
-            print()
+            # print()
             # print("unseenNodes: ", unseenNodes)
-            print("shortest_distance: ", shortest_distance)
-            print("min_distance_node: ", min_distance_node)
-            print("track_predecessor: ", track_predecessor)
-            print('node', node)
-            print()
+            # print("shortest_distance: ", shortest_distance)
+            # print("min_distance_node: ", min_distance_node)
+            # print("track_predecessor: ", track_predecessor)
+            # print('node', node)
+            # print()
             if min_distance_node is None:
                 min_distance_node = node
             elif shortest_distance[node] < shortest_distance[min_distance_node]:
-                print(f"short {shortest_distance[node]} : mindis  {shortest_distance[min_distance_node]}")
+                # print(f"short {shortest_distance[node]} : mindis  {shortest_distance[min_distance_node]}")
                 min_distance_node = node
 
         path_options = graph[min_distance_node].items()
-        print("path_options: ", path_options)
+        # print("path_options: ", path_options)
 
         for child_node, weight in path_options:
-            print('weight + shortest distance[min_distanc_node]' , weight + shortest_distance[min_distance_node])
+            # print('weight + shortest distance[min_distanc_node]' , weight + shortest_distance[min_distance_node])
             if weight + shortest_distance[min_distance_node] < shortest_distance[child_node]:
                 shortest_distance[child_node] = weight + shortest_distance[min_distance_node]
                 track_predecessor[child_node] = min_distance_node
