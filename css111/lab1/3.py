f1fb782e1d9938be56de2482957285e17685f10c
tententgc <nyman3527@gmail.com>
2022-01-16 15:19:36 +0700

    RefactorCode.py

--- css111/lab1/3.py
+++ css111/lab1/3.py
@@ -1,5 +1,17 @@
 import math
 
-a,b,c = [int(x) for x in input().split()]
+a, b, c = [int(x) for x in input().split()]
 
-InSqrt =  (b**2) - (4*a*c) 
+compareValue = (b**2) - (4*a*c)
+
+if compareValue < 0:
+    print("No Solution in real numbers")
+else:
+    ans = math.sqrt(compareValue)
+    x1 = (-b + ans)/(2*a)
+    x2 = (-b - ans)/(2*a)
+
+    if compareValue == 0:
+        print(x1)
+    else:
+        print(x1, x2)
