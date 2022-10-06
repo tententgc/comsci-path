a724f1dbc60a4f1c3f8d1edb835888df7c7a0307
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-10-06 22:58:11 +0700

    [+] lab css112

--- css112-year1/lab-peerajak /problem1.py
+++ css112-year1/lab-peerajak /problem1.py
@@ -1,7 +1,12 @@
 def p1():
     x = input().split()
-    c = input() 
-    ans = [i.count(c) for i in x if c in i]
+    c = input()
+    
+    #ในเคสถ้าไม่มีก็นับ
+    ans = [i.count(x) for i in x] 
+    
+    #เคสมีก็นับจะใช้อันล่าง
+    # ans = [i.count(c) for i in x if c in i]
     print(*ans)
 
 
