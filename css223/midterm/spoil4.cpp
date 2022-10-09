ecdda076f74f59e9b2649cb632b7310df0ef4154
Thanyapisit Buaprakong <nyman3527@gmail.com>
2022-10-09 16:33:48 +0700

    [+] new test

---
+++ css223/midterm/spoil4.cpp
@@ -0,0 +1,79 @@
+#include <iostream> 
+
+using namespace std; 
+
+void merge(int arr[], int l, int m, int r) 
+{ 
+    int i, j, k; 
+    int n1 = m - l + 1; 
+    int n2 = r - m; 
+
+    /* create temp arrays */
+    int L[n1], R[n2]; 
+
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++) 
+        L[i] = arr[l + i]; 
+    for (j = 0; j < n2; j++) 
+        R[j] = arr[m + 1 + j]; 
+
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray 
+    j = 0; // Initial index of second subarray 
+    k = l; // Initial index of merged subarray 
+    while (i < n1 && j < n2) { 
+        if (L[i] <= R[j]) { 
+            arr[k] = L[i]; 
+            i++; 
+        } 
+        else { 
+            arr[k] = R[j]; 
+            j++; 
+        } 
+        k++; 
+    } 
+
+    /* Copy the remaining elements of L[], if there 
+       are any */
+    while (i < n1) { 
+        arr[k] = L[i]; 
+        i++; 
+        k++; 
+    } 
+
+    /* Copy the remaining elements of R[], if there 
+       are any */
+    while (j < n2) { 
+        arr[k] = R[j]; 
+        j++; 
+        k++; 
+    } 
+}
+
+void mergesort(int a[],int low,int high)
+{
+    int mid;
+    if(low<high)
+    {
+        mid=(low+high)/2;
+        mergesort(a,low,mid);
+        mergesort(a,mid+1,high);
+        merge(a,low,mid,high);
+    }
+}
+int main(){ 
+    int a[]={1,9,8,5,3,4,2,6,7,0};
+    int n = sizeof(a)/sizeof(a[0]); 
+    mergesort(a,0,n);
+    for(int i=0;i<10;i++)
+    {
+        cout<<a[i]<<" ";
+    }
+    return 0; 
+}
+
+
+// Language: cpp
+// Path: spoil5.cpp
+// Compare this snippet from spoil4.cpp:
+// BigO(n^2)
